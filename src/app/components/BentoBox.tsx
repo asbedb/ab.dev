4c72bab3e9e90f1dba37cc1698c21d1c405091f8
Asbed B <abb401@gmail.com>
2024-09-21 17:07:17 +1000

    clean up dependencies

--- src/app/components/BentoBox.tsx
+++ src/app/components/BentoBox.tsx
@@ -6,7 +6,7 @@ import GitCard from "./GitCard";
 export default function BentoBox(){
     return(
         <>
-        <div className="grid grid-rows-3 grid-flow-row gap-4 px-4 py-4 leading-10 md:grid-flow-col">
+        <div className="grid grid-rows-3 grid-flow-row gap-4 md:grid-flow-col">
             <div className="p-4 w-full rounded-xl row-span-3"><AboutCard/></div>
             <div className="p-4 w-full rounded-xl col-span-2"><GitCard/></div>
             <div className="p-4 w-full rounded-xl row-span-2 col-span-2">a</div>
