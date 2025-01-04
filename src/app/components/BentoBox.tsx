0a7e83fcc84ff3480e203b2164390ad1d014a0da
Asbed B <abb401@gmail.com>
2025-01-04 15:30:59 +1100

    Updating Layout

--- src/app/components/BentoBox.tsx
+++ src/app/components/BentoBox.tsx
@@ -7,32 +7,29 @@ import PostContainer from "./PostContainer";
 
 export default function BentoBox(){
     return(
-        <>
-            <div className="
-                grid 
-                auto-rows-auto 
-                col-span-1
-                gap-4
-                py-4
-                w-full
-                xl:grid-flow-col
-                xl:col-span-3
-                max-w-[1920px] mx-auto
-                ">
-                    <div className="flex h-fit w-full max-w-full 
-                        col-span-1 
-                        row-span-3 
-                        xl:col-start-2"><AboutCard/></div>
-                    <div className="flex h-full w-full max-w-full 
-                        row-span-1 
-                        col-span-1"><Showcase/></div>
-                    <div className="flex h-fit w-full max-w-full 
-                        xl:col-span-2 
-                        row-span-2 
-                        xl:col-start-3"><Skills/></div>
-                    <div className="flex h-fit w-full max-w-full 
-                        row-span-1 "><PostContainer><LatestPosts/></PostContainer></div>
+    <>
+        <div className="grid h-screen gap-4 p-4 mx-4 grid-flow-col md:col-span-1">
+            <div className="flex rounded-lg h-fit col-start-1">
+                <AboutCard />
             </div>
-        </>
+            
+            {/* Post Container: 30% of rows (spans 2 rows, 2 columns) */}
+            <div className="flex rounded-lg h-fit col-start-1">
+                <PostContainer>
+                    <LatestPosts />
+                </PostContainer>
+            </div>
+
+            {/* Showcase Section: Shares 70% with Skills (spans 2 rows, 1 column) */}
+            <div className="flex rounded-lg h-fit col-start-2">
+                <Showcase />
+            </div>
+            
+            {/* Skills Section: Shares 70% with Showcase (spans 2 rows, 1 column) */}
+            <div className="flex rounded-lg h-1/2">
+                <Skills />
+            </div>
+        </div>
+    </>
     )
 }
