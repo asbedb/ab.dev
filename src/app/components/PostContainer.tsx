8d4fb05a3e2849ed01bc76846dacf9c6e8b45f63
Asbed B <abb401@gmail.com>
2025-01-07 10:55:39 +1100

    mobile layout post container fix

--- src/app/components/PostContainer.tsx
+++ src/app/components/PostContainer.tsx
@@ -10,15 +10,15 @@ const PostContainer: React.FC<PostContainerProps> = ({children}) => {
         <>
             <div className="align-top 
                             grid 
-                            grid-cols-3 
                             grid-flow-row
                             gap-3
                             bg-primary-100
                             p-4 
                             rounded-xl
-                            w-full  
+                            w-full 
+                            2xl:grid-cols-3  
                             ">
-                <div className="text-center text-2xl row-span-1 col-span-3"><Gradient>Latest Updates</Gradient></div>
+                <div className="text-center text-2xl row-span-1 2xl:col-span-3"><Gradient>Latest Updates</Gradient></div>
                 {children}
             </div>
         </>
