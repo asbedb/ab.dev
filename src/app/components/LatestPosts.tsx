56f059a9c3f05f60d941eae552f8193a09566537
Asbed B <abb401@gmail.com>
2025-02-13 15:09:28 +1100

    fixed front end issue for mobile added error to useEffect.

--- src/app/components/LatestPosts.tsx
+++ src/app/components/LatestPosts.tsx
@@ -39,21 +39,21 @@ export default function LatestPosts(){
             }
         }
         fetchGit();
-    }, []);
+    }, [error]);
 
     return(<>                  
                 {postContent.length > 0 ? (
                     postContent.map((post, index) => (
-                        <Link key={index} href={post.link} target="_blank">
-                            <Card className={`bg-primary-200 text-primary-foreground h-60 w-full flex-grow hover:cursor-pointer hover:animate-pulse hover:bg-primary-50`}>
-                                <CardHeader className="flex flex-col items-start align-middle gap-2 content-evenly">
+                        <Link key={index} href={post.link} target="_blank" className="flex flex-col flex-wrap w-full">
+                            <Card className={`bg-primary-200 text-primary-foreground h-60 w-full hover:cursor-pointer hover:animate-pulse hover:bg-primary-50`}>
+                                <CardHeader className="flex flex-col items-start align-middle gap-2 w-full">
                                         {post.pulledFrom == "GitHub"? <FaGithubSquare className="text-secondary-400 text-lg md:text-2xl"/> : <FaMediumM className="text-secondary-400 text-lg md:text-2xl"/>}
-                                        <span className="whitespace-normal truncate line-clamp-1 text-xl md:text-3xl"> {post.pulledFrom == "GitHub"? "🖥️": "📰"}{post.title}</span>
-                                        <h4 className="flex font-bold text-md text-secondary-400 break-words">{post.category || 'None'}</h4>
+                                        <span className="whitespace-normal  text-xl md:text-3xl"> {post.pulledFrom == "GitHub"? "🖥️": "📰"}{post.title}</span>
+                                        <h4 className="flex font-bold text-md truncate line-clamp-1 text-secondary-400 ">{post.category || 'None'}</h4>
                                         <h5>{post.updated_at}</h5>
                                 </CardHeader>
-                                <CardBody className="px-2 text-md text-foreground items-center bg-primary-600">
-                                    <p className="text-primary-foreground px-2 w-full">{post.description}</p>
+                                <CardBody className="flex w-full px-2 text-md text-foreground bg-primary-600">
+                                    <p className="text-primary-foreground px-2 ">{post.description}</p>
                                 </CardBody>
                             </Card>
                         </Link>
--- src/app/components/PostContainer.tsx
+++ src/app/components/PostContainer.tsx
@@ -9,17 +9,16 @@ const PostContainer: React.FC<PostContainerProps> = ({children}) => {
     return(
         <>
             <div className="align-top 
-                            grid 
-                            grid-flow-row
-                            gap-3
+                            flex
+                            flex-col
                             bg-primary-100
                             p-4 
                             rounded-xl
                             w-full 
-                            2xl:grid-cols-3  
                             ">
                 <div className="text-center text-2xl row-span-1 2xl:col-span-3"><Gradient>Latest Updates</Gradient></div>
-                {children}
+                    {children}
+
             </div>
         </>
     )
